30f74aeb01c9402913249ec640912701f7cb6dc3
root <root@master.example.com>
2021-05-26 07:40:59 +0000

    02-RESTfulAPI

---
+++ 02-RESTfulAPI/02-API_ROUTES/main.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify, make_response, request
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+
+
+if __name__ == '__main__':
+#  app.run('0.0.0.0',8081,debug=True)
+  app.run('0.0.0.0',8081)
